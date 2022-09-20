f2e1d580bb1cff33d5ef39d3e348ab4b74ead499
jonesdevelopment <admin@jonesdev.xyz>
2022-09-20 15:52:28 +0200

    Cleaned up some code

--- src/main/java/jones/sonar/bungee/util/logging/ILogger.java
+++ src/main/java/jones/sonar/bungee/util/logging/ILogger.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package jones.sonar.bungee.util.logging;
 
 public interface ILogger {
