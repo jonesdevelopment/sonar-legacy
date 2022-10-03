5e15f32bb6748fa54e750d23ca4f56f5ff755aef
jonesdevelopment <admin@jonesdev.xyz>
2022-10-03 19:53:03 +0200

    Improved DefaultConsoleFilter

--- src/main/java/jones/sonar/bungee/filter/DefaultConsoleFilter.java
+++ src/main/java/jones/sonar/bungee/filter/DefaultConsoleFilter.java
@@ -21,17 +21,11 @@ import jones.sonar.universal.platform.bungee.SonarBungee;
 
 public final class DefaultConsoleFilter {
     protected DefaultConsoleFilter() {
-        SonarBungee.INSTANCE.proxy.getLogger().setFilter(record -> {
-            final String message = SonarBungee.INSTANCE.proxy.getName().equals("BungeeCord") ? (new ConciseFormatter(true))
-                    .formatMessage(record)
-                    .trim() : record.getMessage();
-
-            return ((!(record.getSourceClassName().equals("net.md_5.bungee.connection.InitialHandler")
-                    || (record.getSourceClassName().equals("net.md_5.bungee.log.BungeeLogger") && message.contains(" InitialHandler ")))
-                    && !(record.getSourceClassName().equals("net.md_5.bungee.netty.HandlerBoss")
-                    && message.contains(" - encountered exception: ")))
-                    || Config.Values.LOG_CONNECTIONS)
-                    && !message.equals("No client connected for pending server!");
-        });
+        SonarBungee.INSTANCE.proxy.getLogger().setFilter(record -> ((!(record.getSourceClassName().equals("net.md_5.bungee.connection.InitialHandler")
+                || (record.getSourceClassName().equals("net.md_5.bungee.log.BungeeLogger") && record.getMessage().contains(" InitialHandler ")))
+                && !(record.getSourceClassName().equals("net.md_5.bungee.netty.HandlerBoss")
+                && record.getMessage().contains(" - encountered exception: ")))
+                || Config.Values.LOG_CONNECTIONS)
+                && !record.getMessage().equals("No client connected for pending server!"));
     }
 }
