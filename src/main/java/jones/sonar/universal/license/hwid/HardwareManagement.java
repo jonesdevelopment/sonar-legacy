3fd0b83fb5e9dc2cfcf2cd358f5a5be0ad299b60
jonesdevelopment <admin@jonesdev.xyz>
2022-10-04 18:04:15 +0200

    Fixed HardwareManagement

--- src/main/java/jones/sonar/universal/license/hwid/HardwareManagement.java
+++ src/main/java/jones/sonar/universal/license/hwid/HardwareManagement.java
@@ -19,9 +19,7 @@ package jones.sonar.universal.license.hwid;
 import jones.sonar.universal.util.OperatingSystem;
 import lombok.experimental.UtilityClass;
 
-import java.math.BigInteger;
 import java.net.NetworkInterface;
-import java.security.MessageDigest;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -39,7 +37,7 @@ public class HardwareManagement {
 
         // cache and save all characters out of the network address
         for (final byte character : networkAddress) {
-            networkInformationBuilder.append((char) (character ^ -networkInterface.getName().length()));       // apply some XOR to make it harder for decryption/deobfuscation
+            networkInformationBuilder.append((char) character);
         }
 
         // store the network address dynamically
@@ -51,9 +49,7 @@ public class HardwareManagement {
                 // also include the OS name and arch
                 + OperatingSystem.getOSName()
                 + OperatingSystem.OS_ARCH
-
-                // use the network interface name
-                + networkInterface.getName());
+                + OperatingSystem.OS_VERSION);
     }
 
     private String hash(String information) throws Exception {
@@ -67,28 +63,6 @@ public class HardwareManagement {
         information = Integer.toHexString(base64.hashCode());
 
         // return the hash code of the hardware id
-        return getMD5Hash(information);
-    }
-
-    private String getMD5Hash(final String input) throws Exception {
-
-        // Static getInstance method is called with hashing MD5
-        final MessageDigest digest = MessageDigest.getInstance("MD5");
-
-        // digest() method is called to calculate message digest
-        // of an input digest() return array of byte
-        byte[] messageDigest = digest.digest(input.getBytes());
-
-        // Convert byte array into signum representation
-        final BigInteger bigInteger = new BigInteger(1, messageDigest);
-
-        // Convert message digest into hex value
-        final StringBuilder hashed = new StringBuilder(bigInteger.toString(16));
-
-        while (hashed.length() < 32) {
-            hashed.insert(0, "0");
-        }
-
-        return hashed.toString();
+        return UUID.nameUUIDFromBytes(information.getBytes()).toString();
     }
 }
