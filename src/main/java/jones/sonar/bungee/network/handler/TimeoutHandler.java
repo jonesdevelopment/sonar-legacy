62a237c991c605bfb4a733533eb3a9f0f78e96ae
jonesdevelopment <admin@jonesdev.xyz>
2023-02-04 11:51:31 +0100

    Implemented TimeoutHandler from Hyperion

--- src/main/java/jones/sonar/bungee/network/handler/TimeoutHandler.java
+++ src/main/java/jones/sonar/bungee/network/handler/TimeoutHandler.java
@@ -4,38 +4,48 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.handler.timeout.ReadTimeoutException;
 
 import java.util.concurrent.TimeUnit;
 
 public final class TimeoutHandler extends IdleStateHandler {
     private boolean closed;
 
-    public TimeoutHandler(int timeoutSeconds) {
-        this(timeoutSeconds, TimeUnit.SECONDS);
+    @Deprecated
+    public TimeoutHandler() {
+        this(12L, TimeUnit.SECONDS);
     }
 
-    public TimeoutHandler(long timeout, TimeUnit unit) {
-        super(timeout, 0L, 0L, unit);
+    public TimeoutHandler(final long timeout) {
+        this(timeout, TimeUnit.MILLISECONDS);
+    }
+
+    public TimeoutHandler(final long timeout, final TimeUnit timeUnit) {
+        super(timeout, 0L, 0L, timeUnit);
     }
 
     @Override
-    protected void channelIdle(ChannelHandlerContext ctx, IdleStateEvent evt) throws Exception {
-        assert evt.state() == IdleState.READER_IDLE;
+    protected void channelIdle(final ChannelHandlerContext ctx,
+                               final IdleStateEvent idleStateEvent) throws Exception {
+        assert idleStateEvent.state() == IdleState.READER_IDLE;
 
-        this.readTimedOut(ctx);
+        readTimedOut(ctx);
     }
 
-    private void readTimedOut(ChannelHandlerContext ctx) throws Exception {
-        if (!this.closed) {
-            ctx.fireExceptionCaught(ReadTimeoutException.INSTANCE);
-            ctx.close();
-            this.closed = true;
+    private void readTimedOut(final ChannelHandlerContext ctx) throws Exception {
+        if (!closed) {
+
+            // ==========================================================
+            // The netty (default) ReadTimeoutHandler would normally just throw an Exception
+            // The default ReadTimeoutHandler does only check for the boolean 'closed' and
+            // still throws the Exception even if the channel is closed
+            // This was discovered and fixed by @jones
+            // ==========================================================
+
+            if (ctx.channel().isActive()) {
+                ctx.close();
+            }
+
+            closed = true;
         }
-    }
-
-    @Override
-    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
-        //
     }
 }
