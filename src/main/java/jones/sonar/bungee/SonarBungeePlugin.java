7c8016c0fec06848f26e3593e4364f92f790d71f
jonesdevelopment <admin@jonesdev.xyz>
2023-03-11 13:09:38 +0100

    [remove] unnecessary google annotations

--- src/main/java/jones/sonar/bungee/SonarBungeePlugin.java
+++ src/main/java/jones/sonar/bungee/SonarBungeePlugin.java
@@ -1,6 +1,5 @@
 package jones.sonar.bungee;
 
-import com.google.gson.annotations.SerializedName;
 import jones.sonar.universal.platform.SonarBridge;
 import jones.sonar.universal.platform.SonarPlatform;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -8,19 +7,16 @@ import net.md_5.bungee.api.plugin.Plugin;
 public final class SonarBungeePlugin extends Plugin {
 
     @Override
-    @SerializedName("load")
     public void onLoad() {
         SonarBridge.onLoad(SonarPlatform.BUNGEE, this);
     }
 
     @Override
-    @SerializedName("enable")
     public void onEnable() {
         SonarBridge.onEnable(SonarPlatform.BUNGEE, this);
     }
 
     @Override
-    @SerializedName("disable")
     public void onDisable() {
         SonarBridge.onDisable(SonarPlatform.BUNGEE, this);
     }
